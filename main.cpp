2a91ab37b7c713081d7b0e005e843068796eb861
bom <bom1987@gmail.com>
2018-02-21 12:08:32 +0100

    initial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+int func(int x){
+   x= x*x;
+   // std::cout<<x;
+    return x;
+}
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int n = 2;
+
+    int & z = n;
+    int wynik = func(n);
+    std::cout<<wynik;
     return 0;
+
+
 }
